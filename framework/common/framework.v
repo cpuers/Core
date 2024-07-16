a2ef2ebedcc9cafce386c18a51edb3ff9a71f390
Hongzhi Mao <hongzhimao@smail.nju.edu.cn>
2024-07-16 21:25:33 +0800

    [fw] remove testing files

--- framework/common/framework.v
+++
@@ -1,6 +0,0 @@
-module framework (
-    input x,
-    output y
-);
-    assign y = x;
-endmodule
--- tests/test-test/test.v
+++
@@ -1,6 +0,0 @@
-module test (
-    input x,
-    output y
-);
-    framework u(.x(x), .y(y));
-endmodule
