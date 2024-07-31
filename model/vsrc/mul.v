b8c7cffde10c05217575ccce958fc9644c5e7cac
cyh <221240013@smail.nju.edu.cn>
2024-07-31 20:19:57 +0800

    [mul] pipeline

--- model/vsrc/mul.v
+++ model/vsrc/mul.v
@@ -5,17 +5,34 @@ module mul (
     input [31:0] y,
     input mul_signed,
     input use_high,
-    // input clock, 
-    // input reset,
+    input mul_clk, 
+    input reset,
+    output mul_ok,
     output [31:0] mul_result
-    //output avoidCompileErr
 );
+    reg [31:0] x_r, y_r;
+    reg mul_signed_r, mul_ok;
+    reg status;
+    always @(posedge mul_clk) begin
+        if(reset) begin
+            mul_ok <= 1'b0;
+            status <= 1'b0;
+        end else if(status == 1'b0) begin
+            x_r <= x;
+            y_r <= y;
+            mul_signed_r <= mul_signed;
+            status <= 1'b1;
+        end else if (status <= 1'b1) begin
+            status <= 1'b0;
+            mul_ok <= 1'b1;
+        end
+    end
     //assign avoidCompileErr = clock ^ reset;
     wire [63:0] result;
     WallaceTree32 mywt(
-        .x(x),
-        .y(y),
-        .mul_signed(mul_signed),
+        .x(x_r),
+        .y(y_r),
+        .mul_signed(mul_signed_r),
         .result(result)
     );
 
